d8a481ae599890cda6e4eef031548793adba9977
pingfury <pingfury@outlook.com>
2024-01-25 13:46:37 +0800

    to tauri v2

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+}
+from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
